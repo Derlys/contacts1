1359c67cd48034d1c5391a2cf4cf7a3ec2fe8070
Derlys Dominguez <derlys275@gmail.com>
2023-07-03 12:36:54 +0100

    feat: service contacts

--- src/app/components/list-contacts/list-contacts.component.ts
+++ src/app/components/list-contacts/list-contacts.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ContactsService } from '../../services/contacts.service';
 
 @Component({
   selector: 'app-list-contacts',
   templateUrl: './list-contacts.component.html',
-  styleUrls: ['./list-contacts.component.css']
+  styleUrls: ['./list-contacts.component.css'],
 })
 export class ListContactsComponent {
+  contacts: Contact[] = [];
 
+  constructor(private readonly contactsService: ContactsService) {}
+
+  ngOnInit() {}
+
+  contact: Contact;
+
+  getContacts() {
+    this.contactsService.getContacts().subscribe((contacts) => {
+      this.contacts = contacts;
+      this.getContacts();
+    });
+  }
 }
