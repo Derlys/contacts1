bafc72a51e1f35073ee073285a1829a1c51dfd49
Derlys Dominguez <derlys275@gmail.com>
2023-07-05 19:19:26 +0100

    feat: implemnt detail-contact component

--- src/app/components/detail-contact/detail-contact.component.ts
+++ src/app/components/detail-contact/detail-contact.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Contact } from '../../contact';
+import { AddContactService } from '../../services/add-contact.service';
 
 @Component({
   selector: 'app-detail-contact',
   templateUrl: './detail-contact.component.html',
-  styleUrls: ['./detail-contact.component.css']
+  styleUrls: ['./detail-contact.component.css'],
 })
 export class DetailContactComponent {
+  contacts: Contact[] = [];
 
+  constructor(private readonly addContactService: AddContactService) {
+    this.contacts = this.addContactService.contacts;
+  }
+
+  protected readonly name = name;
 }
